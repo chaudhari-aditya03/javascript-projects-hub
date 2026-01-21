740518c4bbabc9b4cea8f0761170a15602a3c97c
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-21 11:54:26 +0530

    Problem2 is Updated

---
+++ 01-js-foundations/problem8-palindromeNumber.js
@@ -0,0 +1,13 @@
+let num = 123;
+let a = num;
+let reversed = 0;
+while(num > 0) {
+    let rem = num%10;
+    reversed = reversed*10+ rem;
+    num = Math.floor(num/10);
+}
+const palindrome = (a,reversed) => {
+    if(a === reversed) return true;
+    else return false;
+}
+console.log(`Is the number a palindrome? => ${palindrome(a, reversed)}`);

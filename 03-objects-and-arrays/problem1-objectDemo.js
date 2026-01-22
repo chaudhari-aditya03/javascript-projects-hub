433bc29c0dcf6469030d0d05e580376c5cf20cfd
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-22 09:06:12 +0530

    File Updated

---
+++ 03-objects-and-arrays/problem1-objectDemo.js
@@ -0,0 +1,12 @@
+let car = function() {};
+
+let c = new car();
+c.brand = "Honda";
+c.model = "Civic";
+c.year = 2022;
+
+
+console.log(`Car Brand: ${c.brand}`);
+console.log(`Car Model: ${c.model}`);
+console.log(`Car Year: ${c.year}`);
+console.log(`Car Color: ${c.color}`);

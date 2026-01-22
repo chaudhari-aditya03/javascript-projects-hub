7081b14194ffd71824bdba9463893589f90e0ee9
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-22 21:16:01 +0530

    File Uploaded

---
+++ 04-dom-and-browser/domDemo.js
@@ -0,0 +1,8 @@
+    function colourChange(){
+        let HeaderText=document.getElementById("header_text");
+        HeaderText.style.color="red";
+    }
+    function changeText(){
+        let HeaderText=document.getElementById("header_text");
+        HeaderText.innerText="Text Changed Successfully";
+    }

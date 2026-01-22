3b7f403ed0303f0c75247725fc8b32cd31ad1500
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-22 21:15:31 +0530

    File Uploaded

---
+++ 02-functions-and-scope/Basics/variables.js
@@ -0,0 +1,10 @@
+let age = 23;
+// age+5;cd
+console.log(age);
+
+let hindi = 80;
+let eng = 85;
+let math = 90;
+let totalMarks = hindi + eng + math;
+let avg = totalMarks / 3;
+console.log("Average marks: " + avg);

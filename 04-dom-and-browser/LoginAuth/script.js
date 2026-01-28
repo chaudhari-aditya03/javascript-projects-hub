e6a638a729f79b4dce2085943f040c44501b0bfe
chaudhari-aditya03 <adityachaudhariedu312005@gmail.com>
2026-01-28 17:43:40 +0530

    A

--- 04-dom-and-browser/LoginAuth/script.js
+++ 04-dom-and-browser/LoginAuth/script.js
@@ -1,38 +1,7 @@
-// Fixed credentials
-const ADMIN_USERNAME = "admin";
-const ADMIN_PASSWORD = "admin@123";
+const email = "aditya@gmail.com";
+const pass = "aditya@2005";
 
-// Selecting elements
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
-const loginBtn = document.getElementById("loginBtn");
-const message = document.getElementById("message");
-
-// Button click event
-loginBtn.addEventListener("click", function () {
-
-  const enteredEmail = emailInput.value.trim();
-  const enteredPassword = passwordInput.value.trim();
-
-  // Empty validation
-  if (enteredEmail === "" || enteredPassword === "") {
-    message.textContent = " Please fill all fields!";
-    message.style.color = "orange";
-    return;
-  }
-
-  // Credential validation
-  if (enteredEmail === ADMIN_USERNAME && enteredPassword === ADMIN_PASSWORD) {
-    message.textContent = " Login Successful! Welcome Admin.";
-    message.style.color = "green";
-
-    // Optional: Clear fields
-    emailInput.value = "";
-    passwordInput.value = "";
-  } 
-  else {
-    message.textContent = "Invalid Username or Password!";
-    message.style.color = "red";
-  }
-
-});
+let user_email = document.getElementById("e1").value;
+let user_pass = document.getElementById("p1").value;
+console.log(user_email);
+console.log(user_pass);
